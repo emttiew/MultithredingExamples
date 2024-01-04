abb5fc0f7de582d649229114d2b46b330f4f7c8d
Mateusz Wozniak <emttiew@gmail.com>
2024-01-04 17:37:00 +0100

    add explain comments for 04 example

--- examples/04_packaged_task_example.cpp
+++ examples/04_packaged_task_example.cpp
@@ -11,13 +11,13 @@ int sum(int a, int b)
 
 int main()
 {
-    std::packaged_task<int()> task(std::bind(sum, 5, 5));
-    std::future result = task.get_future();
+    std::packaged_task<int()> task(std::bind(sum, 5, 5));  // wrap the function in packaged_task
+    std::future result = task.get_future();                // save the future to use it later in the code
 
-    std::thread task_td(std::move(task));
+    std::thread task_td(std::move(task));                  // move the packaged function to be processed in another thread
     task_td.join();
 
-    std::cout << "result: " <<  result.get() << std::endl;
+    std::cout << "result: " <<  result.get() << std::endl; // get the value using the future object
 
     return 0;
 }
