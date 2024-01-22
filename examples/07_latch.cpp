6daf6c633eacf2e29f65bf231df1f7e4e112406d
Mateusz Wozniak <emttiew@gmail.com>
2024-01-22 20:13:11 +0100

    add latch example

---
+++ examples/07_latch.cpp
@@ -0,0 +1,48 @@
+#include <future>
+#include <latch>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <thread>
+
+void do_work(unsigned num)
+{
+    std::cout << "working on: " << num << std::endl;
+}
+
+void do_more_stuff()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout << "more stuff" << std::endl;
+}
+
+int main()
+{
+    unsigned const thread_count = 3;
+    std::latch work_done(thread_count);
+
+    std::vector<std::future<void>> threads;
+
+    for(unsigned i = 0; i < thread_count; ++i)
+    {
+        threads.push_back(std::async(std::launch::async, [&, i]{
+            do_work(i);
+            work_done.count_down();
+            do_more_stuff();
+        }));
+    }
+
+    work_done.wait();
+    std::cout << "work done" << std::endl;    
+
+    return 0;
+}
+
+// output:
+// working on: 1
+// working on: 2
+// working on: 0
+// work done
+// more stuff
+// more stuff
+// more stuff
